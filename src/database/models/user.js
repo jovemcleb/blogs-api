9e7759154277ad83d4af1d647bdae45eeadb9afb
Caleb Lima <lima.cardoso.caleb@gmail.com>
2022-07-14 19:02:51 -0300

    :sparkles:feat(Req02):adiciona model user

---
+++ src/database/models/user.js
@@ -0,0 +1,20 @@
+"use strict";
+const User = (sequelize, DataTypes) => {
+  const User = sequelize.define(
+    "User",
+    {
+      id: { type: DataTypes.INTEGER, primaryKey: true },
+      displayName: DataTypes.STRING,
+      email: DataTypes.STRING,
+      password: DataTypes.STRING,
+      image: DataTypes.STRING,
+    },
+    {
+      tableName: "Users",
+      timestamps: false,
+    }
+  );
+  return User;
+};
+
+module.exports = User;
